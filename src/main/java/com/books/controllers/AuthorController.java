6ad4ca10f4ab9c6209f93607808566d65d8644a2
Max Braynner <max.braynner@gmail.com>
2019-01-16 22:23:30 -0300

    Refatoração

--- src/main/java/com/books/controllers/AuthorController.java
+++ src/main/java/com/books/controllers/AuthorController.java
@@ -1,7 +1,7 @@
 package com.books.controllers;
 
-import java.util.Optional;
-
+import com.books.models.Author;
+import com.books.repositories.AuthorRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,40 +9,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.books.models.Author;
-import com.books.repositories.AuthorRepository;
-
-import javassist.NotFoundException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
 
-	private AuthorRepository authorRepository;
+    private AuthorRepository authorRepository;
 
-	public AuthorController(AuthorRepository authorRepository) {
-		super();
-		this.authorRepository = authorRepository;
-	}
-	
-	@GetMapping
-	public Iterable<Author> list(){
-		return authorRepository.findAll();
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Author> findById(@PathVariable Long id) throws NotFoundException {
-		Optional<Author> authorOpt = authorRepository.findById(id);
-		
-		ResponseEntity<Author> response;
-		
-		if (authorOpt.isPresent()) {
-			response = new ResponseEntity<Author>(authorOpt.get(), HttpStatus.OK);
-		} else {
-			response = new ResponseEntity<Author>(HttpStatus.NOT_FOUND);
-		}
-		
-		return response;
-	}
-	
+    public AuthorController(AuthorRepository authorRepository) {
+        super();
+        this.authorRepository = authorRepository;
+    }
+
+    @GetMapping
+    public Iterable<Author> list() {
+        return authorRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> findById(@PathVariable Long id) {
+        Optional<Author> authorOpt = authorRepository.findById(id);
+
+        return authorOpt.map(author -> new ResponseEntity<>(author, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 }
--- src/main/java/com/books/controllers/BookController.java
+++ src/main/java/com/books/controllers/BookController.java
@@ -19,7 +19,7 @@ public class BookController {
 	}
 
 	@GetMapping
-	public Iterable<Book> lisBoobks() {
+	public Iterable<Book> listBooks() {
 		return bookRepository.findAll();
 	}
 	
